6615133787bf0dda6666ef3f9b2812a833434270
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-12-01 16:51:30 +0500

    Task3

--- Tyuiu.AtanaevRI.Sprint5.Task3.V1.Lib/DataService.cs
+++ Tyuiu.AtanaevRI.Sprint5.Task3.V1.Lib/DataService.cs
@@ -1,21 +1,28 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System;
 using System.IO;
 using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
 namespace Tyuiu.AtanaevRI.Sprint5.Task3.V1.Lib
 {
     public class DataService : ISprint5Task3V1
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask3.bin";
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+     
             double num1 = Math.Pow(x, 3) - 8;
             double num2 = 2 * Math.Pow(x, 2);
-            double num3 = num1 / num2;
-            string resultString = num3.ToString("F3", System.Globalization.CultureInfo.InvariantCulture);
+            double result = num1 / num2;
+
+            result = Math.Round(result, 3);
+
+   
+            string resultString = result.ToString("F3", System.Globalization.CultureInfo.InvariantCulture);
             File.WriteAllText(path, resultString, Encoding.UTF8);
 
             return path;
         }
-        
     }
 }
