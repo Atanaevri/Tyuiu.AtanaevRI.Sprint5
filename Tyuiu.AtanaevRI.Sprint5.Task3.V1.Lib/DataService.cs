64640c5f0922db0f4c2b9105726158a55671c5ac
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-12-01 16:54:51 +0500

    Task3

--- Tyuiu.AtanaevRI.Sprint5.Task3.V1.Lib/DataService.cs
+++ Tyuiu.AtanaevRI.Sprint5.Task3.V1.Lib/DataService.cs
@@ -9,21 +9,19 @@ namespace Tyuiu.AtanaevRI.Sprint5.Task3.V1.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            // Используем Path.GetTempPath() для получения временной директории
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
 
-            // Вычисление выражения
+            
             double num1 = Math.Pow(x, 3) - 8;
             double num2 = 2 * Math.Pow(x, 2);
             double result = num1 / num2;
 
-            // Округление до трех знаков после запятой
+            
             result = Math.Round(result, 3);
 
-            // Записываем бинарное представление округленного double
             using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create), Encoding.UTF8))
             {
-                writer.Write(result); // BinaryWriter.Write(double) записывает 8 байт double
+                writer.Write(result); 
             }
 
             return path;
