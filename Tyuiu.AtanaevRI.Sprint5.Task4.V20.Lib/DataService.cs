6906417896fe3fc633dda722cbed0cf7668e820b
atanaeva_gg@mail.ru <atanaeva_gg@mail.ru>
2025-12-01 18:03:48 +0500

    Task4

--- Tyuiu.AtanaevRI.Sprint5.Task4.V20.Lib/DataService.cs
+++ Tyuiu.AtanaevRI.Sprint5.Task4.V20.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.AtanaevRI.Sprint5.Task4.V20.Lib
 {
     public class DataService : ISprint5Task4V20
@@ -6,10 +7,14 @@ namespace Tyuiu.AtanaevRI.Sprint5.Task4.V20.Lib
         public double LoadFromDataFile(string path)
         {
             string strx = File.ReadAllText(path);
-            double num1 = Math.Round(Convert.ToDouble(strx), 2);
-            double num2 = Math.Sin(Convert.ToDouble(strx));
+            double value = Convert.ToDouble(strx, CultureInfo.InvariantCulture);
+
+            
+            double num1 = Math.Round(value, 2);
+            double num2 = Math.Sin(value);
             double y = num1 / num2;
-            double res = Math.Round(Math.Pow(y,3), 3);
+            double res = Math.Round(Math.Pow(y, 3), 3);
+
             return res;
         }
     }
